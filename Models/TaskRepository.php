d282ffeab1314ff80c2f544bcdb3a2d0ab0290fe
longphamduc <phamduclong2000192@gmail.com>
2021-03-04 09:17:45 +0700

    add comment

--- Models/TaskRepository.php
+++ Models/TaskRepository.php
@@ -11,26 +11,46 @@ class TaskRepository
     public function __construct() {
         $this->taskRepository = new TaskResourceModel('tasks', 'id', new TaskModel);
     }
+
+    /**
+     * @param object $model
+     * @return bool
+     */
     public function add($model)
     {
         return $this->taskRepository->save($model);
     }
 
+    /**
+     * @param object $model
+     * @return bool
+     */
     public function edit($model)
     {
         return $this->taskRepository->save($model);
     }
 
+    /**
+     * @param object $model
+     * @return bool
+     */
     public function delete($model)
     {
         return $this->taskRepository->delete($model->id);
     }
 
+    /**
+     * @param string/int $id
+     * @return object
+     */
     public function get($id)
     {
         return $this->taskRepository->find($id);
     }
 
+    /**
+     * @return object
+     */
     public function getAll()
     {
         return $this->taskRepository->all();
